725a2b87d15ffc994998198996f461aaaefb4bd1
ashishmandal029 <135951869+ashishmandal029@users.noreply.github.com>
2025-06-16 09:56:15 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,21 +3,21 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_TOKEN = os.environ.get("7672885270:AAHHvb0DKvju3OVVslKCLJp-rjY5N_He2KE")
 
-    SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
+    SESSION_NAME = os.environ.get("stkuserbot", ":memory:")
 
-    API_ID = int(os.environ.get("API_ID"))
+    API_ID = int(os.environ.get("24871620"))
 
-    API_HASH = os.environ.get("API_HASH")
+    API_HASH = os.environ.get("e4195bedc71234a179a3d9ac0cad6401")
 
-    CLIENT_ID = os.environ.get("CLIENT_ID")
+    CLIENT_ID = os.environ.get("1030398326032-lplhdgttff6qjgrs30bgs24a5kav1hl8.apps.googleusercontent.com")
 
-    CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+    CLIENT_SECRET = os.environ.get("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")
 
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    BOT_OWNER = int(os.environ.get("5728398903"))
 
-    AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "")
+    AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", "5728398903")
 
     AUTH_USERS = [BOT_OWNER, 374321319] + (
         [int(user.strip()) for user in AUTH_USERS_TEXT.split(",")]
